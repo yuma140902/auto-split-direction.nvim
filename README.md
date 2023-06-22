8977d8f3489fc7bf7b617c8c76a04f99fef6e748
yuma14 <yuma140902@gmail.com>
2023-06-22 11:30:22 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# auto-split-direction.nvim
+
+TODO
---
+++ lua/auto-split-direction/init.lua
@@ -0,0 +1,31 @@
+local function auto_split(ratio)
+  local width = vim.api.nvim_win_get_width(0)
+  local height = vim.api.nvim_win_get_height(0)
+  vim.notify("width = " .. tostring(width) .. " height = " .. tostring(height))
+  if width > height * ratio then
+    vim.cmd 'vsplit'
+  else
+    vim.cmd 'split'
+  end
+end
+
+local function setup(opts)
+  if opts == nil then
+    opts = {}
+  end
+
+  if opts['debug'] then
+    vim.notify("auto-split-direction has started!", vim.log.levels.ERROR)
+  end
+
+  local ratio = opts['ratio']
+  if ratio == nil then
+    ratio = 2.0
+  end
+
+  vim.api.nvim_create_user_command('Split', function() auto_split(ratio) end, {})
+end
+return {
+  setup = setup,
+  auto_split = auto_split
+}
